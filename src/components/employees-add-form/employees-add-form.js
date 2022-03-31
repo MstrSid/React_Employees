e46f49baeaaa8900186e5b297b10f7b85af006e1
Eugene Kos <mstrsid@live.com>
2022-03-31 12:26:14 +0300

    done

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,47 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Добавьте нового сотрудника</h3>
-			<form
-				className="add-form d-flex">
-				<input type="text"
-					className="form-control new-post-label"
-					placeholder="Как его зовут?" />
-				<input type="number"
-					className="form-control new-post-label"
-					placeholder="З/П в $?" />
+class EmployeesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: undefined,
+			currency: 'USD'
+		}
+	}
 
-				<button type="submit"
-					className="btn btn-outline-light">Добавить</button>
-			</form>
-		</div>
-	)
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	render() {
+		const { name, salary, currency } = this.state;
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form
+					className="add-form d-flex">
+					<input type="text" name='name'
+						className="form-control new-post-label"
+						placeholder="Как его зовут?" onChange={this.onValueChange} value={name} />
+					<input type="number" name='salary'
+						className="form-control new-post-label"
+						placeholder="З/П?" onChange={this.onValueChange} value={salary} />
+					<select name='currency'
+						className="form-control new-post-label currecy-dropdown" onChange={this.onValueChange} value={currency}>
+						<option value={"USD"}>USD</option>
+						<option value={"EUR"}>EUR</option>
+						<option value={"CHN"}>CHN</option>
+					</select>
+					<button type="submit"
+						className="btn btn-outline-light">Добавить</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default EmployeesAddForm;
